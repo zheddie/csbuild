103e978c8e380e05087dab588eac93d7054ef5c7
Gavin Zhang <zheddie@163.com>
2016-01-23 19:13:42 +0800

    Remove some unnecessary comment lines.
    Remove some printf lines originally used for debug. These lines are no use for production.

--- csbuild.pl
+++ csbuild.pl
@@ -5,7 +5,6 @@ if(not $target eq ""){
 	$dir = `finddir $target`;
 	$csdir = `findcsdir $target`;
 	#update the code from CMVC side.
-#	system("copyCMVC2CS.pl $target fix");
 	@splts=split(/\/jre\/lib\/ppc/,"$dir");
 	$rootdir=$splts[0];
 	if( -f "$rootdir/src.zip"){
@@ -21,13 +20,10 @@ if(not $target eq ""){
 			system("unzip -d $dir/srczip/ $rootdir/src.zip > /dev/null");
 		}
 	}
-#	print ("rootdir=$rootdir\n");
 	system("crtcsfiles.pl $target");  # //cscope.files would be created for cscope use.
 	chdir($csdir);
 #	system("cscope -bvq ");
 	system("cscope -Rbvq ");
-	#print("csdir=$csdir\n");
-#chdir($dir);
 	#system("ctags --verbose=yes -h \".h.hpp.H.inc\" -L cscope.files");
 	system("ctags -h \".h.hpp.H.inc\" -L cscope.files");
 }
